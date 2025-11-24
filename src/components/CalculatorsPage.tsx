95ab2637a297677db70f70b8b4e777bd346a2fda
mocher1 <48806962+mocher1@users.noreply.github.com>
2025-11-24 09:38:34 +0100

    Added CalculatorsPage.tsx

---
+++ src/components/CalculatorsPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calculators from './Calculators';
+
+const CalculatorsPage = () => {
+  return (
+    <main className="pt-14 min-h-screen bg-mind-surface-content-white font-sans">
+      <Calculators />
+    </main>
+  );
+};
+
+export default CalculatorsPage;
+
+export default CalculatorsPage
