deb07e59107bd0e38fd7a61027540cebbf874a05
mocher1 <48806962+mocher1@users.noreply.github.com>
2025-12-18 14:43:40 +0100

    Fix extra closing div tag in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,6 @@ const About = () => {
                  </a>
               </div>
             </div>
-          </div>
           
           {/* ZDJĘCIE / WIZUALIZACJA */}
           <div className={`order-1 lg:order-2 relative transition-all duration-1000 delay-200 ${isVisible ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-8'}`}>
